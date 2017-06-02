7655271afa9c67c1cedec80763443c6cba1d791b
Arwa <arwa.it15@gmail.com>
2017-06-02 13:58:14 -0500

    part2

---
+++ overly-excited.js
@@ -0,0 +1,24 @@
+// Create an array that contains the words in the sentence
+var sentence = ["hello", "how","are","you","today", "hello", "how","are","you","today"];
+
+// The addExcitement function should accept the array as the sole argument
+function addExcitement (sentence) 
+{
+	var builder = " ";
+    for(var i=1; i<=sentence.length; i++)
+    {
+    	if(i%3 === 0)
+    	{
+    		builder += ` ${sentence[i-1]}!`;
+    		console.log(builder);
+    	}
+       	else
+       	{
+    	builder += ` ${sentence[i-1]}`;
+    	console.log(builder);
+        }
+    }
+}
+
+// Invoke the function and pass in the array
+addExcitement(sentence);
